ac81f08108297c46a642ca2e9e124f1e82eb2ba2
Ina Bjornestad <iinusen7@gmail.com>
2019-08-28 21:31:13 +0200

    should work

--- BNDM.py
+++ BNDM.py
@@ -1,7 +1,6 @@
 
 # Input: two strings T (length n) and P (length m)
 # Output: The locations of all occurences of P in T 
-# incidence_matrix(G, nodelist=None, edgelist=None, oriented=False, weight=None) ? 
 
 def BNDM(Text, Pattern):
     print("pre-processing...")
@@ -10,41 +9,37 @@ def BNDM(Text, Pattern):
     m = len(Pattern)
     r = 26 # alphabet size
 # pre-processing: compute incidence matrix B of P 
-    B = [[ 0 for i in range(r) ] for j in range(m)]
+    B = [[ 0 for i in range(m) ] for j in range(m)]
     B = {(rx, cx): c for rx, r in enumerate(B)\
                 for cx, c in enumerate(r)}
-    s = 0x1 
-    q = int(format(0, '0'+str(m)))
-    for i in range (0, m):
-        key_exists = 0x1
-        if Pattern[i] in B: 
-            key_exists = 0x0 
-        B[Pattern[i]] = s | key_exists
+    s = 1 
+    for i in range (m-1, -1, -1):
+        key_exists = 0
+        if Pattern[m-i-1] in B: 
+            key_exists = B[Pattern[m-i-1]]
+        B[Pattern[m-i-1]] = s | key_exists
         s <<= 1
-        #q = int(format(0, '0'+ str(m)))   
-    print(B)
 
 # search phase
     print("searching...")
-    i = m
-    while i <= n:
-        j = -1
-        last = 0
+    pos = 0
+    while (pos <= n-m):
+        j = m - 1
+        last = m 
         d = ~0
-        D = 1 ^ m
-        W=Text[i - m]
-        while D != 0 ^ m:
-            j = j + 1
-            D = D & B[W]
-            if D == 0^m:
-                break
-            if j == m:
-                print("Occurence of T: " + Pattern[i-m] + " at pos " + str(i-m))
-                break
-            if j < m:
-                last = j
-            D = D << 1
-        i = i + (m - last)
-
+        while (d != 0 and j >= 0):
+            if Text[pos+j] in B: 
+                d = d & B[Text[pos+j]]    
+            else:
+                d = d & 0
+            j = j - 1
+            if d != 0:
+                if (j >= 0):
+                    last = j
+                else:
+                    print(pos)
+            d = d << 1
+        pos = pos + last
+        
 if __name__ == "__main__": 
     BNDM("acggtgac", "tgac")
